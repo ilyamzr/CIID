ac6c76c5d008e190f28d049bfdebe22f362092f0
Ilya <35830175@study.bsuir.by>
2024-11-30 13:03:13 +0300

    .

--- Контейнеры_очереди_стеки/3/src/main/java/org/example/Deque.java
+++ Контейнеры_очереди_стеки/3/src/main/java/org/example/Deque.java
@@ -1,88 +1,98 @@
 package org.example;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.Scanner;
 
 public class Deque<I> {
-
-    private static final Logger logger = Logger.getLogger(Deque.class.getName());
 
     private Node head;
     private Node tail;
     private int size;
 
-    private class Node{
+    private class Node {
         I item;
         Node next;
         Node prev;
     }
 
-    public boolean isEmpty()
-    {
+    public Deque() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void pushLeft(I item)
-    {
-        Node oldHead = head;
-        head = new Node();
-        head.next = oldHead;
-        head.item = item;
-        head.prev = null;
-        if (!isEmpty())
-        {
-            oldHead.prev = head;
+    public void pushLeft(I item) {
+        Node newHead = new Node();
+        newHead.item = item;
+        newHead.next = head;
+        newHead.prev = null;
+
+        if (isEmpty()) {
+            tail = newHead;
+        } else {
+            head.prev = newHead;
         }
-        else head.next = null;
-        size++;
-    }
-    public void pushRight(I item)
-    {
-        Node oldTail = tail;
-        tail = new Node();
-        tail.prev = oldTail;
-        tail.next = null;
-        tail.item = item;
-        if(!isEmpty())
-        {
-            oldTail.next = tail;
-        }
-        else tail.prev = null;
+        head = newHead;
         size++;
     }
 
-    public I popLeft()
-    {
-        if (!isEmpty())
-        {
-            I item = head.item;
-            head = head.next;
-            head.prev = null;
-            size--;
-            return item;
+    public void pushRight(I item) {
+        Node newTail = new Node();
+        newTail.item = item;
+        newTail.prev = tail;
+        newTail.next = null;
+
+        if (isEmpty()) {
+            head = newTail;
+        } else {
+            tail.next = newTail;
         }
-        else return null;
+        tail = newTail;
+        size++;
     }
-    public I popRight()
-    {
-        if (!isEmpty())
-        {
-            I item = tail.item;
-            tail = tail.prev;
-            tail.next = null;
-            size--;
-            return item;
+
+    public I popLeft() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Дек пуст");
         }
-        else return null;
+        I item = head.item;
+        head = head.next;
+
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        size--;
+        return item;
+    }
+
+    public I popRight() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Дек пуст");
+        }
+        I item = tail.item;
+        tail = tail.prev;
+
+        if (tail != null) {
+            tail.next = null;
+        } else {
+            head = null;
+        }
+        size--;
+        return item;
     }
 
     public void printDeque() {
         Node current = head;
         while (current != null) {
-            logger.info(current.item + " ");
+            System.out.print(current.item + " ");
             current = current.next;
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -90,38 +100,45 @@ public class Deque<I> {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            logger.info("1) Вставить в начало\n2) Вставить в конец\n3) Удалить из начала\n4) Удалить из конца\n5) Закончить");
+            System.out.println("1) Вставить в начало\n2) Вставить в конец\n3) Удалить из начала\n4) Удалить из конца\n5) Закончить");
             int choice = Integer.parseInt(scanner.nextLine());
             int item;
             switch (choice) {
                 case 1:
-                    logger.info("Введите число: ");
+                    System.out.print("Введите число: ");
                     item = Integer.parseInt(scanner.nextLine());
                     deque.pushLeft(item);
                     break;
                 case 2:
-                    logger.info("Введите число: ");
+                    System.out.print("Введите число: ");
                     item = Integer.parseInt(scanner.nextLine());
                     deque.pushRight(item);
                     break;
                 case 3:
-                    item = deque.popLeft();
-                    logger.log(Level.INFO, String.format("Удален элемент: %d", item));
+                    try {
+                        item = deque.popLeft();
+                        System.out.println("Удален элемент: " + item);
+                    } catch (IllegalStateException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 4:
-                    item = deque.popRight();
-                    logger.log(Level.INFO, String.format("Удален элемент: %d", item));
+                    try {
+                        item = deque.popRight();
+                        System.out.println("Удален элемент: " + item);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 5:
-                    logger.info("Выход...");
+                    System.out.println("Выход...");
                     scanner.close();
                     return;
                 default:
-                    logger.info("Неверный выбор. Пожалуйста, попробуйте снова.");
+                    System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
             }
-            logger.info("Текущее состояние дека: ");
+            System.out.println("Текущее состояние дека: ");
             deque.printDeque();
         }
     }
 }
-
